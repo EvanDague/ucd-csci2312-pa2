2f68c4877e0d74ba26589b64fb4f140661dfbfd7
EvanDague <Evan.Dague@ucdenver.edu>
2016-05-08 22:09:44 -0600

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,225 +1,277 @@
 #include "Point.h"
 
+#include <iostream>
+#include <sstream>
+using namespace std;
+
 namespace Clustering{
   
-Point::Point(int dim){
-  __dim = dim;
-  for(int i = 0; i < dim; i++){
-    setValue(i, 0); 
+  unsigned int Point::__idGen;
+  
+  Point::Point(int dim):
+    __dim(dim)
+  {
+    __values = new double[__dim];
+    __id = ++__idGen;
+    for(int i = 0; i < dim; i++){
+      __values[i] = 0;
+    }
   }
-}
-
-Point::Point(int dim, double *value){
-  __dim = dim;
-  for(int i = 0; i < dim; i++){
-    setValue(i, *value);
+  
+  Point::Point(int dim, double *value):
+    __dim(dim)
+  {
+    __values[dim];
+    __values = new double[__dim];
+    __id = ++__idGen;
+    for(int i = 0; i < dim; i++){
+      __values[i] = 0;
+    }
   }
-}
-
-// Big three: cpy ctor, overloaded operator=, dtor
-Point::Point(const Point & copy){
+  
+  // Big three: cpy ctor, overloaded operator=, dtor
+  Point::Point(const Point & copy){
+    double value;
+        
+    __id = copy.getId();
+    __dim = copy.getDims();
+    __values = new double[__dim];
+    for(int i = 0; i < __dim; i++){
+      __values[i] = copy.__values[i];
+    }
+  }
+  
+  Point& Point::operator=(const Point & a){
+    for(int i = 0; i < a.getDims(); i++){
+      __values[i] = a.getValue(i);
+    }
+    
+    return *this;
+  }
+  
+  Point::~Point(){
+    delete[] __values;
+  }
+  
+  // Accessors & mutators
+  int Point::getId() const{
+    return __id;
+  }
+  
+  int Point::getDims() const{
+    return __dim;
+  }
+  
+  void Point::setValue(int num, double values){
+    __values[num] = values;
+  }
+  
+  double Point::getValue(int num) const{
+    return __values[num];
+  }
+  
+  // Functions
+  double Point::distanceTo(const Point &a) const{
+      double dist = 0;
+      
+      for(int i = 0; i < a.getDims(); i++){
+          dist += (pow(__values[i] - a.__values[i],2));
+      }
+      
+      return sqrt(dist);
+  }
+  
+  // Overloaded operators
+  
+  // Members
+  Point& Point::operator*=(double num){ // p *= 6; p.operator*=(6)
+    int dim = getDims();
     double value;
     
-  __id = copy.getId();
-  __dim = copy.getDims();
-  for(int i = 0; i < __dim; i++){
-    setValue(i,copy.getValue(i));
+    for(int i = 0; i < dim; i++){
+      value = getValue(i) * num;
+      __values[i] = value;
+    }
   }
-}
-
-Point& Point::operator=(const Point & a){
-  for(int i = 0; i < getDims(); i++){
-    setValue(i, a.getValue(i));
-  }
-}
-
-Point::~Point(){
-}
-
-// Accessors & mutators
-int Point::getId() const{
-  return __id;
-}
-
-int Point::getDims() const{
-  return __dim;
-}
-
-void Point::setValue(int num, double values){
-  __values[num] = values;
-}
-
-double Point::getValue(int num) const{
-  return __values[num];
-}
-
-// Functions
-double Point::distanceTo(const Point & a) const{
-    double dist;
+  
+  Point& Point::operator/=(double num){
+    int dim = getDims();
+    double value;
     
-    for(int i = 0; i < a.getDims(); i++){
-        dist += (pow(a.getValue(i),2));
+    for(int i = 0; i < dim; i++){
+      value = getValue(i) / num;
+      __values[i] = value;
+    }
+  }
+  
+  const Point Point::operator*(double num) const{ // prevent (p1 * 2) = p2;
+    int dim = getDims();
+    double value;
+    
+    Point newVal(dim);
+    
+    for(int i = 0; i < dim; i++){
+      value = getValue(i) * num;
+      newVal.setValue(i, value);
     }
     
-    return sqrt(dist);
-}
-
-// Overloaded operators
-
-// Members
-Point& Point::operator*=(double num){ // p *= 6; p.operator*=(6)
-  int dim = getDims();
-  double value;
-  
-  for(int i = 0; i < dim; i++){
-    value = getValue(i) * num;
-    setValue(i, value);
-  }
-}
-
-Point& Point::operator/=(double num){
-  int dim = getDims();
-  double value;
-  
-  for(int i = 0; i < dim; i++){
-    value = getValue(i) / num;
-    setValue(i, value);
-  }
-}
-
-const Point Point::operator*(double num) const{ // prevent (p1 * 2) = p2;
-  int dim = getDims();
-  double value;
-  
-  Point newVal(dim);
-  
-  for(int i = 0; i < dim; i++){
-    value = getValue(i) * num;
-    newVal.setValue(i, value);
+    return newVal;
   }
   
-  return newVal;
-}
-
-const Point Point::operator/(double num) const{ // p3 = p2 / 2;
-  int dim = getDims();
-  double value;
-  
-  Point newVal(dim);
-  
-  for(int i = 0; i < dim; i++){
-    value = getValue(i) / num;
-    newVal.setValue(i, value);
-  }
-  
-  return newVal;
-}
-
-double& Point::operator[](int index){
-    __values[index];
-}
-
-// Friends
-Point& operator+=(Point & a, const Point & b){
-  if(a.getDims() == b.getDims()){
-        for(int i = 0; i < a.getDims(); i++){
-            a.setValue(i, a.getValue(i) + b.getValue(i));
-        }
+  const Point Point::operator/(double num) const{ // p3 = p2 / 2;
+    int dim = getDims();
+    double value;
+    
+    Point newVal(dim);
+    
+    for(int i = 0; i < dim; i++){
+      value = getValue(i) / num;
+      newVal.setValue(i, value);
     }
-}
-
-Point& operator-=(Point & a, const Point & b){
-  if(a.getDims() == b.getDims()){
-        for(int i = 0; i < a.getDims(); i++){
-            a.setValue(i, a.getValue(i) - b.getValue(i));
-        }
-    }
-}
-
-const Point operator+(const Point & a, const Point & b){
+    
+    return newVal;
+  }
+  
+  double& Point::operator[](int index){
+      return __values[index];
+  }
+  
+  // Friends
+  Point& operator+=(Point & a, const Point & b){
     if(a.getDims() == b.getDims()){
-        for(int i = 0; i < a.getDims(); i++){
-            return a.getValue(i)+b.getValue(i);
-        }
-    }
-}
-
-const Point operator-(const Point & a, const Point & b){
-if(a.getDims() == b.getDims()){
-        for(int i = 0; i < a.getDims(); i++){
-            return a.getValue(i)-b.getValue(i);
-        }
-    }
-}
-
-bool operator==(const Point & a, const Point & b){
-  if(a.distanceTo(a) == b.distanceTo(b)){
-    return true;
+          for(int i = 0; i < a.getDims(); i++){
+              a[i] += b.getValue(i);
+          }
+      }
+    return a;
   }
-  else{
-    return false;
-  }
-}
-
-bool operator!=(const Point & a, const Point & b){
-  if(a.distanceTo(a) != b.distanceTo(b)){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-bool operator<(const Point & a, const Point & b){
-  if(a.distanceTo(a) < b.distanceTo(b)){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-bool operator>(const Point &a , const Point & b){
-  if(a.distanceTo(a) > b.distanceTo(b)){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-bool operator<=(const Point &a , const Point & b){
-  if(a.distanceTo(a) <= b.distanceTo(b)){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-bool operator>=(const Point & a, const Point & b){
-  if(a.distanceTo(a) >= b.distanceTo(b)){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-std::ostream &operator<<(std::ostream & outData, const Point & a){
-    for(int i = 0; i < a.getDims(); i++){
-        outData << a.getValue(i);
-    }
-}
-
-std::istream &operator>>(std::istream & inData, Point & a){
-  double value = 0;
-  int i = 0;
   
-  while(inData){
-    inData >> value;
-    a.setValue(i, value);
-    i++;
+  Point& operator-=(Point & a, const Point & b){
+    if(a.getDims() == b.getDims()){
+          for(int i = 0; i < a.getDims(); i++){
+              a[i] -= b.getValue(i);
+          }
+    }
+    return a;
   }
-}
+  
+  const Point operator+(const Point & a, const Point & b){
+    Point holder(a.getDims());
+    
+    if(a.getDims() == b.getDims()){
+          for(int i = 0; i < a.getDims(); i++){
+              holder[i] = a.getValue(i) + b.getValue(i);
+          }
+     }
+    return holder;
+  }
+  
+  const Point operator-(const Point & a, const Point & b){
+    Point holder(a.getDims());
+    
+    if(a.getDims() == b.getDims()){
+          for(int i = 0; i < a.getDims(); i++){
+              holder[i] = a.getValue(i) - b.getValue(i);
+          }
+      }
+    return holder;
+  }
+  
+  bool operator==(const Point & a, const Point & b){
+    if(a.getDims() != b.getDims()){
+      return false;
+    }
+    
+    for(int i = 0; i < a.__dim; i++){
+      if(a.__values[i] != b.__values[i]){
+        return false;
+      }
+    }
+    
+    return true;
+  }
+  
+  bool operator!=(const Point & a, const Point & b){
+    if(a.getDims() != b.getDims()){
+      return true;
+    }
+    
+    for(int i = 0; i < a.__dim; i++){
+      if(a.__values[i] != b.__values[i]){
+        return true;
+      }
+    }
+    
+    return false;
+  }
+  
+  bool operator<(const Point & a, const Point & b){
+    double adist, bdist;
+    for(int i = 0; i < a.getDims(); i++){
+      adist += a.getValue(i) * a.getValue(i);
+    }
+    for(int i = 0; i < b.getDims(); i++){
+      adist += a.getValue(i) * a.getValue(i);
+    }
+    
+    return bdist < adist;
+  }
+  
+  bool operator>(const Point &a , const Point & b){
+    double adist, bdist;
+    for(int i = 0; i < a.getDims(); i++){
+      adist += a.getValue(i) * a.getValue(i);
+    }
+    for(int i = 0; i < b.getDims(); i++){
+      adist += a.getValue(i) * a.getValue(i);
+    }
+    
+    return bdist > adist;
+    }
+  
+  bool operator<=(const Point &a , const Point & b){
+    double adist, bdist;
+    
+    for(int i = 0; i < a.getDims(); i++){
+      adist += a.getValue(i) * a.getValue(i);
+    }
+    for(int i = 0; i < b.getDims(); i++){
+      bdist += b.getValue(i) * b.getValue(i);
+    }
+    
+    return bdist >= adist;
+  }
+  
+  bool operator>=(const Point & a, const Point & b){
+    double adist, bdist;
+    for(int i = 0; i < a.getDims(); i++){
+      adist += a.getValue(i) * a.getValue(i);
+    }
+    for(int i = 0; i < b.getDims(); i++){
+      bdist += b.getValue(i) * b.getValue(i);
+    }
+    
+    return bdist <= adist;
+    }
+
+  std::ostream &operator<<(std::ostream & outData, const Point & a){
+      for(int i = 0; i < a.getDims(); i++){
+          outData << a.__values[i] << ", ";
+      }
+      return outData;
+  }
+  
+  std::istream &operator>>(std::istream & inData, Point & a){
+    string holder;
+    double d;
+    
+    for(int i = 0; i < a.__dim; i++)
+    {
+        getline(inData, holder, ',');
+        std::stringstream input(holder);
+        input>>d;
+        a.__values[i] = d;
+    }
+    return inData;
+  }
 }
